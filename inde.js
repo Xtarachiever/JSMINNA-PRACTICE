3921a531ce578794d111f7bfadf25806897545a5
Adeyemi Esther <63353240+Xtarachiever@users.noreply.github.com>
2020-10-29 15:41:32 +0100

    Task8

---
+++ inde.js
@@ -0,0 +1,9 @@
+const navSlide=()=>{
+    const burger=document.querySelector('#btn');
+    const nav=document.querySelector('.nav-links');
+
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active')
+    });
+}
+navSlide();
